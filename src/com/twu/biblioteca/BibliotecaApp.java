2bfdcec1cc4490ff333998458f7240c909a0e273
sukreet <sukreetr@thoughtworks.com>
2015-06-12 03:57:08 +0530

    Integrated file system with biblioteca app

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,18 +1,20 @@
 package com.twu.biblioteca;
 
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        ArrayList<Book> books = new ArrayList<>();
-        books.add(new Book("Head First Java", "Kathy", 1995));
-        books.add(new Book("Learning C", "John", 2000));
-
+        ArrayList<Book> books;
+        FileHandler readBooks = new FileHandler("listOfBooks.txt");
+//        books.add(new Book("Head First Java", "Kathy", 1995));
+//        books.add(new Book("Learning C", "John", 2000));
+        books = readBooks.readListOfBooks();
 
         BibliotecaFunctionalities bibliotecaFunctionalities = new BibliotecaFunctionalities();
         Menu menu = new Menu(books);

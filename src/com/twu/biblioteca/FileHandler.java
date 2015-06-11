440958f66bc64e0db22299ff1939ae3a13006d25
sukreet <sukreetr@thoughtworks.com>
2015-06-12 03:28:09 +0530

    passed spec for reading data from file and creating book list

--- src/com/twu/biblioteca/FileHandler.java
+++ src/com/twu/biblioteca/FileHandler.java
@@ -1,5 +1,45 @@
 package com.twu.biblioteca;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class FileHandler {
+    String fileName;
+
+    public FileHandler(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public ArrayList<Book> readListOfBooks() throws IOException {
+        ArrayList<Book> listOfBooks = new ArrayList<>();
+        String name, author, year;
+        int yearOfPublication;
+       // boolean checkout;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                name = line.substring(0, 39);
+                name = name.trim();
+                author = line.substring(40, 79);
+                author = author.trim();
+                year = line.substring(80, 89);
+                year = year.trim();
+                yearOfPublication = Integer.parseInt(year);
+
+               // checkout = Boolean.parseBoolean(line.substring(90, 98));
+                listOfBooks.add(new Book(name, author, yearOfPublication));
+
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+        }
+        return listOfBooks;
+    }
 }
--- test/com/twu/biblioteca/FileHandlerTest.java
+++ test/com/twu/biblioteca/FileHandlerTest.java
@@ -1,12 +1,28 @@
 package com.twu.biblioteca;
 
+
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
 public class FileHandlerTest {
-
     @Test
-    public void shouldReadBookListCorrectlyFromFile() {
+    public void shouldReadListOfBooksWorks() throws IOException {
+        String testFileName = "listOfBooks.txt";
 
+        FileHandler readBooks = new FileHandler(testFileName);
+
+        ArrayList<Book> actualListOfBooks = readBooks.readListOfBooks();
+
+        ArrayList<Book> expectedListOfBooks = new ArrayList<Book>();
+        expectedListOfBooks.add(new Book("Macbeth", "William Shakespear", 1611));
+        expectedListOfBooks.add(new Book("Julius Caesar", "William Shakespear", 1599));
+
+        assertThat(actualListOfBooks, is(expectedListOfBooks));
     }
-
 }
